f16246113938aa4f8379d5f8726ec588bb243da4
Thanh-Quy Nguyen <nguyen.tq@hotmail.fr>
2021-10-15 19:00:43 +0200

    :wrench: Ropsten network configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,18 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+const alchemy_api_key = process.env.alchemy_api_key;
+const private_key = process.env.private_key;
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
 	solidity: "0.8.0",
+	networks: {
+		ropsten: {
+			url: `https://eth-ropsten.alchemyapi.io/v2/${alchemy_api_key}`,
+			accounts: [`0x${private_key}`],
+		},
+	},
 };
